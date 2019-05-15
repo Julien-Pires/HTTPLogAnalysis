e05aa9631f6861e68984d6e9bebae4d01c98d09f
Julien Pires <piresjulien95@gmail.com>
2019-05-15 17:04:12 +0200

    Add projects

---
+++ src/HTTPAnalysis/HTTPAnalysis.Monitoring/Data.fs
@@ -0,0 +1,15 @@
+﻿namespace HTTPAnalysis.Monitoring
+
+open System
+
+type RequestMethod =
+    | POST
+    | GET
+
+type Request = {
+    Address : string
+    Method : RequestMethod
+    Section : string 
+    Date : DateTime
+    StatusCode : int16
+    ResponseSize : int }
