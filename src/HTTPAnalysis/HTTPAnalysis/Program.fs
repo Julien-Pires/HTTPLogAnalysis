8adaf860160b21f1f98174fd529a0ef327cd54b7
Julien Pires <piresjulien95@gmail.com>
2019-05-16 22:54:35 +0200

    Update log parser

--- src/HTTPAnalysis/HTTPAnalysis/Program.fs
+++ src/HTTPAnalysis/HTTPAnalysis/Program.fs
@@ -1,8 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open FSharp.Control
+open HTTPAnalysis.Monitoring
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    File.readContinuously "C:\Users\Takumi\Desktop\Foo.txt" 
+    |> AsyncSeq.iter (fun c -> Console.WriteLine(LogParser.parse c))
+    |> Async.Start
+
+    Console.ReadLine() |> ignore
     0 // return an integer exit code
